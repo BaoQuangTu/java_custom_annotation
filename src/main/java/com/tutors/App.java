d194a8cd75b1163aad08b12f939435dd39f7f513
baoquangtu <baoquangtu150707@gmail.com>
2020-12-01 08:32:41 +0700

    optimize

--- src/main/java/com/tutors/App.java
+++ src/main/java/com/tutors/App.java
@@ -10,7 +10,7 @@ public class App {
             throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
         Student student = new Student("1", "Bảo Quang Tử", "Mê Linh - Hà Nội", 24);
         MappingConverter<Student> mapping = new MappingConverter<Student>(Student.class);
-        Map<String, String> mapFieldValue = mapping.buildMapFieldValue(student);
+        Map<String, String> mapFieldValue = mapping.getFieldValueByObject(student);
         System.out.println(mapFieldValue);
         System.out.println(mapFieldValue.get("name"));
         System.out.println(mapFieldValue.get("address"));
